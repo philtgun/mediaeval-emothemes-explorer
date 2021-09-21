a79887d1380c6ba8e823e28d3edc5059f8bb9b82
Philip Tovstogan <phil.tgun@gmail.com>
2021-09-21 13:03:41 +0200

    Make graph responsive

--- docs/scripts.js
+++ docs/scripts.js
@@ -4,13 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
         data.type = 'heatmap';
         data.colorscale = 'YlGnBu';
         data.hovertemplate = 'Tag: %{x}<br>Team: %{y}<br>PR-AUC: %{z:.4f}<extra></extra>';
-            // '<i>Price</i>: $%{y:.2f}\' +\n' +
-            // '                        \'<br><b>X</b>: %{x}<br>\' +\n' +
-            // '                        \'<b>%{text}</b>\','
-        console.dir(data);
+        // console.dir(data);
         Plotly.newPlot(plotElement, [data], {
             title: "PR-AUC values per tag for all submissions",
             margin: {l: 320, b: 100, t: 40},
+        }, {
+            responsive: true,
         });
     });
 });
