b5259679dacc43cef72eea633cacc4300d7aa079
Philip Tovstogan <phil.tgun@gmail.com>
2021-09-21 13:00:58 +0200

    Improve tooltips

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let plotElement = document.getElementById('plot');
+    const plotElement = document.getElementById('plot');
     Plotly.d3.json('./data.json', data => {
         data.type = 'heatmap';
         data.colorscale = 'YlGnBu';
+        data.hovertemplate = 'Tag: %{x}<br>Team: %{y}<br>PR-AUC: %{z:.4f}<extra></extra>';
+            // '<i>Price</i>: $%{y:.2f}\' +\n' +
+            // '                        \'<br><b>X</b>: %{x}<br>\' +\n' +
+            // '                        \'<b>%{text}</b>\','
         console.dir(data);
-        Plotly.newPlot(plotElement, [data], {'margin': {'l': 320, 'b': 100, 't': 40}});
+        Plotly.newPlot(plotElement, [data], {
+            title: "PR-AUC values per tag for all submissions",
+            margin: {l: 320, b: 100, t: 40},
+        });
     });
 });
